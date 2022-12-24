ed7bd417b065e76368f1e07121b5b5ad2c5e379c
Никита <49557048+nikitavasilev03@users.noreply.github.com>
2022-12-24 09:44:01 +0300

    add files

---
+++ script.js
@@ -0,0 +1,56 @@
+const slider = {
+  slideNumber: 4,
+  progress: 280,
+};
+
+let start = 0;
+
+window.addEventListener("DOMContentLoaded", () => {
+  console.log(`DOMContentLoaded-Event; Time: ${Date.now() - start} ms`);
+});
+
+window.addEventListener("load", () => {
+  console.log(`load-Event; Time: ${Date.now() - start} ms`);
+
+  const sliderLoaderElements = document.getElementsByClassName(
+    "header-wrapper--slider-line-loader"
+  );
+  const sliderLineElements = document.getElementsByClassName(
+    "header-wrapper--slider-line"
+  );
+  const sliderTitleElements = document.getElementsByClassName(
+    "header-wrapper--slider-title"
+  );
+
+  const sliderRun = () => {
+    if (slider.progress >= 280) {
+      sliderLoaderElements[slider.slideNumber - 1].style.width = `${0}%`;
+      sliderLineElements[slider.slideNumber - 1].classList.remove(
+        "header-wrapper--slider-line-active"
+      );
+      sliderTitleElements[slider.slideNumber - 1].classList.remove(
+        "header-wrapper--slider-title-active"
+      );
+
+      slider.progress = 0;
+      slider.slideNumber++;
+      if (slider.slideNumber > 4) slider.slideNumber = 1;
+
+      sliderLineElements[slider.slideNumber - 1].classList.add(
+        "header-wrapper--slider-line-active"
+      );
+      sliderTitleElements[slider.slideNumber - 1].classList.add(
+        "header-wrapper--slider-title-active"
+      );
+    }
+
+    sliderLoaderElements[
+      slider.slideNumber - 1
+    ].style.width = `${slider.progress}px`;
+
+    slider.progress++;
+    setTimeout(() => sliderRun(), 20);
+  };
+
+  sliderRun();
+});
